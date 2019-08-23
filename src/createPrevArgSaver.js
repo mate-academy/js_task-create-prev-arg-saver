1b56c16add577fa6494bdeef466d8b001cfb8850
Volodsher <volodsher@gmail.com>
2019-08-23 17:38:17 +0300

    again refactoring

--- src/createPrevArgSaver.js
+++ src/createPrevArgSaver.js
@@ -1,25 +1,13 @@
 'use strict';
 
-/**
- * Write a function (factory) creating a function (device) returning its first
- * argument from previous call. The first call should return undefined
- *
- * For example:
- *
- * const argSaver = createPrevArgSaver()
- * argSaver(123) // return undefined
- * argSaver(456) // return 123
- * argSaver() // return 456
- * argSaver(789) // return undefined
- *
- * @param arg
- */
 function createPrevArgSaver(arg) {
-  const previousArg = [];
+  let nowArg = arg;
 
-  return (number) => {
-    previousArg.push(number);
-    return previousArg[previousArg.length - 2];
+  return function(number) {
+    const previousArg = nowArg;
+    nowArg = number;
+
+    return previousArg;
   };
 }
 
