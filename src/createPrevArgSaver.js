f19b0fb13e575ddcc095159fa660edd5db4bd4a3
OlehTereshchuk <olehtereshchuk@ukr.net>
2019-11-25 12:25:52 +0200

    solution

--- src/createPrevArgSaver.js
+++ src/createPrevArgSaver.js
@@ -16,6 +16,12 @@
  */
 function createPrevArgSaver(arg) {
   // write code here
+  let previous;
+  return function(next) {
+    const result = previous;
+    previous = next;
+    return result;
+  };
 }
 
 module.exports = createPrevArgSaver;
