69d0d5d03dff349b94282b2585c2522f111da816
Vova Pyvovar <ditinapivo@gmail.com>
2020-02-03 23:53:15 +0200

    solution

--- src/createPrevArgSaver.js
+++ src/createPrevArgSaver.js
@@ -15,7 +15,15 @@
  * @param arg
  */
 function createPrevArgSaver(arg) {
-  // write code here
+  let previousArg;
+
+  return function(nextArg) {
+    const newArg = previousArg;
+
+    previousArg = nextArg;
+
+    return newArg;
+  };
 }
 
 module.exports = createPrevArgSaver;
